7bf4c06a795e2ee700c26eb5b004d3440acd631a
Yosselin Flores <yoshy.floresg17@gmail.com>
2023-01-31 15:49:15 -0600

    Practica semana 3

---
+++ porgrafirstapp/src/views/ExampleView.vue
@@ -0,0 +1,14 @@
+<template>
+    <example-component title="Programacion IV"></example-component>
+</template>
+<script>
+// @ is an alias to /src
+import exampleComponent from "@/components/exampleComponent.vue";
+
+export default {
+    name: "ExampleView",
+    components: {
+    exampleComponent,
+},
+};
+</script>
