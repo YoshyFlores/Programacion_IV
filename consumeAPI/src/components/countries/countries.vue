5f06bb7a898e64f5a54e9a57e06af725d20e0e28
Yosselin Flores <yoshy.floresg17@gmail.com>
2023-02-16 21:36:34 -0600

    Avance de proyecto, semana 5.

--- consumeAPI/src/components/countries/countries.vue
+++ consumeAPI/src/components/countries/countries.vue
@@ -1,29 +1,56 @@
 <template>
-    {{data.map((d) => d.region)}}
+    {{ loadData.map( r => r.region) }}
 </template>
 
 <script>
-import requests from './request.js';
+import requests from './requests';
 export default({
-    data() {
+    data(){
         return {
             data: [],
+            firstIndex: 0,
+            lastIndex: 0,
+            sliceData: []
         }
     },
     methods: {
         async getAllCountries(){
             const response = await fetch(requests.getAllCountries);
-            this.data=await response.json();
-
+            this.data = await response.json();
+            this.total = this.data.length;
         },
         async getbyRegion(){
-            const response = await fetch(requests.getbyRegion('europe'));
+            const response =
+            await fetch(requests.getbyRegion(this.$store.state.currentRegion));
             this.data = await response.json();
+            this.total = this.data.length;
         }
     },
-    created() {
-        //this.getAllCountries
-        this.getbyRegion();
-    },
+    computed: {
+        loadData(){
+            if(this.$store.state.currentRegion == "All"){
+                this.getAllCountries()
+                this.lastIndex = this.$store.state.currentPage * 10;
+                this.firstIndex = this.lastIndex - 10;
+                this.sliceData = this.data.slice(this.firstIndex, this.lastIndex)
+            } else {
+               this.getbyRegion()
+               this.lastIndex = this.$store.state.currentPage * 10;
+               this.firstIndex = this.lastIndex - 10;
+               this.sliceData = this.data.slice(this.firstIndex, this.lastIndex)
+
+            }
+            return this.sliceData
+        },
+        total: {
+            get(){
+                return this.$store.state.total
+            },
+            set(value) {
+                this.$store.commit("SET_TOTAL", value)
+            }
+        }
+    }
 });
+
 </script>
--- consumeAPI/src/components/countries/filters.vue
+++ consumeAPI/src/components/countries/filters.vue
@@ -2,23 +2,34 @@
     <div>
         <h3>Filtros por region</h3>
         <div v-for="region in allRegions" :key="region.id">
-            <input type="radio" name="selectRegion"
-            :value="region">
+            <input type="radio" name="selectRegion" class="form-check-input"
+            :value="region"
+            v-model="currentRegion"
+            >
             {{region}}
-            </div>
-    </div>
-
-
+        </div>
+    </div><br />
+    {{ $store.state.currentRegion }}<br /><br />
 </template>
+
 <script>
-export default{
-    data(){
+export default {
+    data() {
         return{
             allRegions:
-            ['All', 'Europe', 'Africa', 'Asia', 'Americas', 'Oceania', 'Antartic']
+            ['All', 'Europe', 'Asia', 'Africa','Americas',
+            'Oceania', 'Antarctic']
+        }
+    },
+    computed: {
+        currentRegion: {
+            get() {
+                return this.$store.state.currentRegion
+            },
+            set(value) {
+                this.$store.commit("SET_REGION", value)
+            }
         }
     }
-};
-
-
+}
 </script>
